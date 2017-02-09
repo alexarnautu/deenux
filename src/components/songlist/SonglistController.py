56859aad39fa3bc4f2949175d81d9451ee8ba835
Marius Aiordăchioaei <aiordachioaei.marius@gmail.com>
2017-02-09 10:52:16 +0200

    Fixed button on deselection

--- src/components/songlist/SonglistController.py
+++ src/components/songlist/SonglistController.py
@@ -19,7 +19,7 @@ class SonglistController:
 
     def on_line_selected(self, selected, deselected):
         item_selected = len(selected.indexes()) > 0
-        self.context.player.play_pause_button.setEnabled(item_selected)
+        self.context.player.play_pause_button.setEnabled(item_selected or self.context.player.active)
         if item_selected:
             self.context.player.to_play = self.view.songlist_model.table_data[selected.indexes()[0].row()][0]
             self.context.player.current_mix = self.view.songlist_model
