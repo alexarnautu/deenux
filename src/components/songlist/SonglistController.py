dfd12cfc9fd4b430ad78928a0902c7cde7f94ae1
mirko <mirceadino97@gmail.com>
2017-02-12 16:29:58 +0200

    Pulled out Controller.

--- src/components/songlist/SonglistController.py
+++ src/components/songlist/SonglistController.py
@@ -1,18 +1,10 @@
+from src.components.Controller import Controller
 
 
-class SonglistController:
+class SonglistController(Controller):
 
     def __init__(self, view, context):
-        self._context = context
-        self._view = view
-    
-    @property
-    def view(self):
-        return self._view
-
-    @property
-    def context(self):
-        return self._context
+        super(SonglistController, self).__init__(view, context)
 
     def on_line_double_click(self, index):
         self.context.deezer.jukebox.start(index.model().table_data[index.row()][0])
--- src/components/toolbar/ToolbarController.py
+++ src/components/toolbar/ToolbarController.py
@@ -1,15 +1,7 @@
+from src.components.Controller import Controller
 
 
-class ToolbarController:
-
-    @property
-    def context(self):
-        return self._context
-
-    @property
-    def view(self):
-        return self._view
+class ToolbarController(Controller):
 
     def __init__(self, view, context):
-        self._context = context
-        self._view = view
+        super(ToolbarController, self).__init__(view, context)
