7196a40b791e838f31e8b1b3401b02af5533390a
Marius Aiordăchioaei <aiordachioaei.marius@gmail.com>
2017-02-13 09:11:51 +0200

    Implemented worker on controller base

--- src/components/Controller.py
+++ src/components/Controller.py
@@ -1,4 +1,9 @@
+from PyQt5.QtCore import QRunnable, QThreadPool
+
+
 class Controller:
+
+    thread_pool = QThreadPool()
 
     def __init__(self, view=None, context=None):
         self._view = view
@@ -11,3 +16,22 @@ class Controller:
     @property
     def view(self):
         return self._view
+
+    class Worker(QRunnable):
+        
+        def __init__(self, fn, args, kwargs):
+            super().__init__()
+            self.__fn = fn
+            self.__args = args
+            self.__kwargs = kwargs
+
+        def run(self):
+            return self.__fn(*self.__args, **self.__kwargs)
+
+    @staticmethod
+    def async(decorated):
+
+        def kick_off (*args, **kwargs):
+            Controller.thread_pool.start(Controller.Worker(decorated, args, kwargs))
+
+        return kick_off
